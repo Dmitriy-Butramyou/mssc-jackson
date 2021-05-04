71fa4cb7d18b9244307ac46869b576af98bb48e3
D.Butramyou <d.butramyou@gmail.com>
2021-05-04 14:36:43 +0300

    Initial commit

---
+++ src/test/java/home/springframework/msscjackson/MsscJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package home.springframework.msscjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MsscJacksonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
